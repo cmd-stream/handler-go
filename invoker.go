c9831a705138fe74739fa0c515691d78a2a41bcf
yra <ymz.ncnk@outlook.com>
2025-01-24 17:12:29 +0000

    Add InvokerFn

--- invoker.go
+++ invoker.go
@@ -14,3 +14,12 @@ type Invoker[T any] interface {
 	Invoke(ctx context.Context, at time.Time, seq base.Seq, cmd base.Cmd[T],
 		proxy base.Proxy) error
 }
+
+// InvokerFn functional implementation of the Invoker interface.
+type InvokerFn[T any] func(ctx context.Context, at time.Time, seq base.Seq,
+	cmd base.Cmd[T], proxy base.Proxy) error
+
+func (i InvokerFn[T]) Invoke(ctx context.Context, at time.Time, seq base.Seq,
+	cmd base.Cmd[T], proxy base.Proxy) error {
+	return i(ctx, at, seq, cmd, proxy)
+}
