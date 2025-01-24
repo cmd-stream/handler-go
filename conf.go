6017dfad432bea544eb10c9e27794be72c44d051
yra <ymz.ncnk@outlook.com>
2025-01-24 17:11:26 +0000

    Change Conf.ReceiveTimeout to Conf.CmdReceiveTimeout

--- conf.go
+++ conf.go
@@ -4,12 +4,12 @@ import "time"
 
 // Conf is a configuration of the Handler.
 //
-// If the Handler does not receive any commands during ReceiveTimeout, the
+// If the Handler does not receive any commands during CmdReceiveTimeout, the
 // corresponding connection will be closed. A timeout value of 0 means it will
 // wait indefinitely.
 // If At is set to true, the Handler will call Invoker.Invoke() with the
 // command's reception time.
 type Conf struct {
-	ReceiveTimeout time.Duration
-	At             bool
+	CmdReceiveTimeout time.Duration
+	At                bool
 }
