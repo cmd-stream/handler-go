7442175f1c5b2b5bd7e6508f7cf043c39d2d92a1
yra <ymz.ncnk@outlook.com>
2025-01-26 20:44:11 +0000

    Correct comments

--- invoker.go
+++ invoker.go
@@ -9,11 +9,11 @@ import (
 
 // Invoker is used by the server to execute Commands.
 //
-// A single Invoker instance is shared by all Workers (each Worker handles one
-// client connection at a time), so it must be thread-safe.
+// A single Invoker instance is used to execute Commands in separate goroutines,
+// so it must be thread-safe.
 //
 // Invoke method can serve as a central point to handle operations that are
-// common across multiple Commands, such as logging.
+// common across multiple Commands (such as logging).
 type Invoker[T any] interface {
 	Invoke(ctx context.Context, at time.Time, seq base.Seq, cmd base.Cmd[T],
 		proxy base.Proxy) error
